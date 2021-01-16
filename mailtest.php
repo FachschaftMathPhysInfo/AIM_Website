be11e12a32074e938c650a4663a26f89ab513d2f
Oliver <mehling@mathphys.stura.uni-heidelberg.de>
2021-01-16 15:26:04 +0100

    Test script for mail via SMTP

---
+++ mailtest.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(TRUE);
+
+try {
+    $mail->IsSMTP();
+    $mail->Encoding = 'base64';
+    $mail->CharSet = 'UTF-8';
+    $mail->Host = 'relay.urz.uni-heidelberg.de';
+    //$mail->SMTPDebug  = 0; 
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587; 
+
+    $mail->addReplyTo('aim@mathphys.stura.uni-heidelberg.de', 'Fachschaft MathPhysInfo – fAIM');
+    $mail->setFrom('aim@mathphys.stura.uni-heidelberg.de', 'Fachschaft MathPhysInfo – fAIM');
+    $mail->addAddress('mehling@mathphys.stura.uni-heidelberg.de');
+    //$mail->addBCC('aim@mathphys.stura.uni-heidelberg.de');
+    $mail->Subject = 'Bestätigung Ihrer fAIM-Anmeldung';
+    $mail->Body = "Sehr geehrte Damen und Herren,\n\nvielen Dank für Ihre Anmeldung zur fAIM 2020/21. Ihre Daten wurden erfolgreich in unsere Datenbank eingetragen. Weitere Informationen senden wir Ihnen in Kürze.\n\nMit freundlichen Grüßen,\nDie Fachschaft MathPhysInfo";
+    $mail->send();
+}
+catch (Exception $e)
+{
+    echo $e->errorMessage();
+}
+catch (\Exception $e)
+{
+    /* PHP exception */
+    echo $e->getMessage();
+}
+?>
+</body>
+</html>
